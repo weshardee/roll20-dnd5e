714fa91efb371dfa29e3661fe87d115b881b6a9b
Wes Hardee <whardee@nerdery.com>
2015-01-23 17:52:16 -0600

    refactor for gulp-hb

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,51 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var src = 'src/';
 var dest = 'web/';
 
-var htmlSrc = src + '*.html';
-var cssSrc = src + '*.css';
+var glob = {
+    html: src + '*.hbs',
+    partials: src + '**/*.css',
+    css: src + '*.css'
+};
+
+var hbOptions = {
+    partials: [glob.partials]
+};
+
+// TODO: maybe break tasks up with gulp-load
 
 gulp.task('html', function() {
-    return gulp.src(htmlSrc)
-    .pipe(gulp.dest(dest));
+    return gulp.src(glob.html)
+    .pipe(plugins.frontMatter())
+    .pipe(plugins.hb(hbOptions))
+    .pipe(plugins.ext.replace('html'))
+    .pipe(gulp.dest(dest))
+    .pipe(reload({stream:true}));
 });
 
 gulp.task('css', function() {
-    return gulp.src(cssSrc)
-    .pipe(gulp.dest(dest));
+    return gulp.src(glob.css)
+    .pipe(gulp.dest(dest))
+    .pipe(reload({stream:true}));
 });
 
-gulp.task('default', ['html', 'css']);
+gulp.task('build', ['html', 'css']);
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch(glob.html, ['html']);
+    gulp.watch(glob.css, ['css']);
+});
+
+gulp.task('browser-sync', ['watch'], function() {
+    browserSync({
+        server: {
+            baseDir: dest
+        }
+    });
+});
+
+gulp.task('default', ['browser-sync']);
